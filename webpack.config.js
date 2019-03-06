21a64713abdafc2c994beffb607c4b7f8da9f74b
dhoko <aurelien@protonmail.com>
2019-03-06 18:05:39 +0100

    Activate hot reload for external components
    react-components, etc.

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,9 @@ const plugins = require("./webpack/plugins");
 const optimization = require("./webpack/optimization");
 const { outputPath } = require("./webpack/paths");
 
+const { excludeNodeModulesExcept, excludeFiles, createRegex } = require('./webpack/helpers/regex');
+const { BABEL_EXCLUDE_FILES, BABEL_INCLUDE_NODE_MODULES } = require('./webpack/constants');
+
 const conf = {
   isProduction: false
 };
@@ -16,7 +19,14 @@ const config = {
   bail: isProduction,
   devtool: false,
   watchOptions: {
-    ignored: [/node_modules/, "i18n/*.json", /\*\.(gif|jpeg|jpg|ico|png)/]
+    ignored: [
+        createRegex(
+            excludeNodeModulesExcept(BABEL_INCLUDE_NODE_MODULES),
+            excludeFiles(BABEL_EXCLUDE_FILES)
+        ),
+        "i18n/*.json",
+        /\*\.(gif|jpeg|jpg|ico|png)/
+    ]
   },
   devServer: {
     hot: true,
