6128bec8531cbb7dce4a8c008d6832d03961019c
mmso <mmso@mmso.se>
2019-03-22 14:35:19 +0100

    Review with @dhoko

--- webpack/helpers/source.js
+++ webpack/helpers/source.js
@@ -2,19 +2,23 @@ const path = require('path');
 const ROOT_DIR = process.cwd();
 const portfinder = require('portfinder'); // Coming from webpack-dev-server
 
-const getPort = () => process.env.NODE_ENV_PORT || 8080;
-const findPort = async () => {
+const getPort = ({ port }) => port || process.env.NODE_ENV_PORT || 8080;
+
+const findPort = async (basePort) => {
     // Default PORT for webpack
-    portfinder.basePort = getPort();
+    portfinder.basePort = basePort;
     const port = await portfinder.getPortPromise();
     process.env.NODE_ENV_PORT = port;
     return port;
 };
+
+const getPublicPath = ({ publicPath }) => publicPath || process.env.PUBLIC_PATH || '/';
 
 const getSource = (input) => path.join(ROOT_DIR, input);
 
 module.exports = {
     getPort,
     findPort,
-    getSource
+    getSource,
+    getPublicPath
 };
