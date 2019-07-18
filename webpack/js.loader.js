aa9c21d429b0dc9058a7776dada7a76eac775b95
vincaslt <vincaslt@gmail.com>
2019-07-18 16:52:33 +0300

    Basic typescript setup

--- webpack/js.loader.js
+++ webpack/js.loader.js
@@ -1,114 +1,130 @@
-const { excludeNodeModulesExcept, excludeFiles, createRegex } = require('./helpers/regex');
-const { BABEL_EXCLUDE_FILES, BABEL_INCLUDE_NODE_MODULES } = require('./constants');
+const {
+  excludeNodeModulesExcept,
+  excludeFiles,
+  createRegex
+} = require("./helpers/regex");
+const {
+  BABEL_EXCLUDE_FILES,
+  BABEL_INCLUDE_NODE_MODULES
+} = require("./constants");
 
-const BABEL_PLUGINS_PRODUCTION = [['babel-plugin-transform-react-remove-prop-types', { removeImport: true }]];
+const BABEL_PLUGINS_PRODUCTION = [
+  ["babel-plugin-transform-react-remove-prop-types", { removeImport: true }]
+];
 
 const UNSUPPORTED_JS_LOADER = [
-    {
-        loader: 'babel-loader',
-        options: {
-            cacheDirectory: true,
-            cacheCompression: true,
-            compact: true,
-            presets: [
-                [
-                    '@babel/preset-env',
-                    {
-                        targets: { browsers: ['ie 11'] },
-                        useBuiltIns: 'entry',
-                        corejs: 3
-                    }
-                ]
-            ],
-            plugins: []
-        }
+  {
+    loader: "babel-loader",
+    options: {
+      cacheDirectory: true,
+      cacheCompression: true,
+      compact: true,
+      presets: [
+        [
+          "@babel/preset-env",
+          {
+            targets: { browsers: ["ie 11"] },
+            useBuiltIns: "entry",
+            corejs: 3
+          }
+        ]
+      ],
+      plugins: []
     }
+  }
 ];
 
 module.exports = ({ isProduction }, flow) => {
-    const TRANSPILE_JS_LOADER = [
-        {
-            loader: 'babel-loader',
-            options: {
-                cacheDirectory: true,
-                cacheCompression: isProduction,
-                compact: isProduction,
-                presets: [
-                    [
-                        '@babel/preset-env',
-                        {
-                            targets: {
-                                browsers: isProduction
-                                    ? [
-                                          // TODO: Only browsers that support es6
-                                          '> 0.5%, not IE 11, Firefox ESR'
-                                      ]
-                                    : ['last 1 chrome version', 'last 1 firefox version', 'last 1 safari version']
-                            },
-                            useBuiltIns: 'entry',
-                            corejs: 3,
-                            exclude: ['transform-typeof-symbol'] // Exclude transforms that make all code slower
-                        }
-                    ],
-                    [
-                        '@babel/preset-react',
-                        {
-                            // Adds component stack to warning messages
-                            // Adds __self attribute to JSX which React will use for some warnings
-                            development: !isProduction
-                        }
+  const TRANSPILE_JS_LOADER = [
+    {
+      loader: "babel-loader",
+      options: {
+        cacheDirectory: true,
+        cacheCompression: isProduction,
+        compact: isProduction,
+        presets: [
+          [
+            "@babel/preset-env",
+            {
+              targets: {
+                browsers: isProduction
+                  ? [
+                      // TODO: Only browsers that support es6
+                      "> 0.5%, not IE 11, Firefox ESR"
                     ]
-                ],
-                plugins: [
-                    '@babel/plugin-syntax-dynamic-import',
-                    '@babel/plugin-proposal-object-rest-spread',
-                    ['@babel/plugin-proposal-class-properties', { loose: true }],
-                    require('babel-plugin-lodash'),
-                    '@babel/plugin-transform-runtime',
-                    ...(isProduction ? BABEL_PLUGINS_PRODUCTION : ['react-hot-loader/babel'])
-                ]
+                  : [
+                      "last 1 chrome version",
+                      "last 1 firefox version",
+                      "last 1 safari version"
+                    ]
+              },
+              useBuiltIns: "entry",
+              corejs: 3,
+              exclude: ["transform-typeof-symbol"] // Exclude transforms that make all code slower
             }
-        }
-    ];
-
-    if (flow === 'i18n') {
-        TRANSPILE_JS_LOADER[0].options.plugins.push([
-            'ttag',
+          ],
+          [
+            "@babel/preset-react",
             {
-                extract: {
-                    output: 'i18n/template.pot'
-                }
+              // Adds component stack to warning messages
+              // Adds __self attribute to JSX which React will use for some warnings
+              development: !isProduction
             }
-        ]);
-        return [
-            {
-                test: /\.js$/,
-                exclude: createRegex(
-                    excludeNodeModulesExcept(BABEL_INCLUDE_NODE_MODULES),
-                    excludeFiles(BABEL_EXCLUDE_FILES)
-                ),
-                use: TRANSPILE_JS_LOADER
-            }
-        ];
+          ],
+          ["@babel/preset-typescript"]
+        ],
+        plugins: [
+          "@babel/plugin-syntax-dynamic-import",
+          "@babel/plugin-proposal-object-rest-spread",
+          ["@babel/plugin-proposal-class-properties", { loose: true }],
+          require("babel-plugin-lodash"),
+          "@babel/plugin-transform-runtime",
+          ...(isProduction
+            ? BABEL_PLUGINS_PRODUCTION
+            : ["react-hot-loader/babel"])
+        ]
+      }
     }
+  ];
 
-    return [
-        {
-            test: /\.js$/,
-            use: ['source-map-loader'],
-            enforce: 'pre'
-        },
-        {
-            test: /unsupported\.js$/,
-            use: UNSUPPORTED_JS_LOADER
-        },
-        {
-            test: /\.js$/,
-            exclude: createRegex(
-                excludeNodeModulesExcept(BABEL_INCLUDE_NODE_MODULES),
-                excludeFiles([...BABEL_EXCLUDE_FILES, 'unsupported.js'])
-            ),
-            use: TRANSPILE_JS_LOADER
+  if (flow === "i18n") {
+    TRANSPILE_JS_LOADER[0].options.plugins.push([
+      "ttag",
+      {
+        extract: {
+          output: "i18n/template.pot"
         }
+      }
+    ]);
+    return [
+      {
+        test: /\.js$|\.tsx?$/,
+        exclude: createRegex(
+          excludeNodeModulesExcept(BABEL_INCLUDE_NODE_MODULES),
+          excludeFiles(BABEL_EXCLUDE_FILES)
+        ),
+        use: TRANSPILE_JS_LOADER
+      }
     ];
+  }
+
+  return [
+    {
+      test: /\.js$|\.tsx?$/,
+      use: ["source-map-loader"],
+      enforce: "pre"
+    },
+    {
+      test: /unsupported\.(js|tsx?)$/,
+      use: UNSUPPORTED_JS_LOADER
+    },
+    {
+      test: /\.js$|\.tsx?$/,
+      exclude: createRegex(
+        excludeNodeModulesExcept(BABEL_INCLUDE_NODE_MODULES),
+        excludeFiles([...BABEL_EXCLUDE_FILES, "unsupported.js"])
+      ),
+      use: TRANSPILE_JS_LOADER
+    }
+  ];
 };
