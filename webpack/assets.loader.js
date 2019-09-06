a4cf3450de42eb15b0c3cc77c5083c13e139db42
mmso <mmso@mmso.se>
2019-09-06 18:03:14 +0200

    Add comment

--- webpack/assets.loader.js
+++ webpack/assets.loader.js
@@ -4,6 +4,21 @@ const DESIGN_SYSTEM_CSS_SVG = 'sprite-for-css-only.svg';
 
 module.exports = () => [
     {
+        /**
+         * oneOf allows to take the first match instead of all matches, .e.g
+         *
+         * without one of:
+         *
+         * sprite-icons.svg -> [svg-inline-loader, url-loader, file-loader]
+         * img-1.svg -> [url loader, file loader]
+         * design-system-icon.svg -> file loader
+         *
+         * with one of:
+         *
+         * sprite-icons.svg -> svg-inline-loader
+         * img-1.svg -> url loader
+         * design-system-icon.svg -> file loader
+         */
         oneOf: [
             {
                 test: new RegExp(`${DESIGN_SYSTEM_ICONS_SVG}$`),
